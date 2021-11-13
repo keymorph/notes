65f31b0fbb9ab8df63e503788ae8ff0d6f3f9ff0
danteedmonson <danteedmonson@gmail.com>
2021-11-13 15:56:52 -0500

    note taking update

--- backend/APIs/register.js
+++ backend/APIs/register.js
@@ -1,33 +1,63 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bodyParser = require('body-parser');
-var mysql = require('mysql'); // import mysql module
-const connection = require('../connection');
+const bcrypt = require("bcrypt");
+var mysql = require("mysql"); // import mysql module
+const connection = require("../connection");
 
+router.post("/register", async (req, res) => {
+  const { email, password } = req.body;
 
- 
+  /*  Email and password validation */
+  //Check if email and password fields exist
+  if (!email || !password) {
+    return res.status(400).json({ error: "Empty field in form input." });
+  }
+  // Check for valid email
+  if (email.length < 5 || email.length > 50 || !email.includes("@")) {
+    return res.status(400).json({ error: "Invalid email address." });
+  }
+  // Check for valid password
+  if (password.length < 6 || password.length > 40) {
+    return res
+      .status(400)
+      .json({ error: "Password must be between 6 and 40 characters." });
+  }
 
+  let exists = [];
 
-router.post('/register', async (req, res) => {
+  // Search for account in database
+  connection.query(
+    `SELECT * FROM users WHERE email = '${email}';`,
+    async (err, results) => {
+      if (err) throw err;
 
-    console.log(connection)
+      exists = await results;
 
-
-    try {
-        console.log("IM HERE")
-
-        connection.query(`INSERT INTO users (email, password) VALUES ('${req.body.email}', '${req.body.password}');`, (err, results) => {
-            if (err) throw err;
-            console.log(results)
-        })
-       
-        res.send("hello")
-
-    } catch (err) {
-        res.send("err")
-
+      // check if account already exist
+      if (exists.length != 0) {
+        return res
+          .status(409)
+          .json({ error: "That account doesn't exist. Enter a different email." }); // 409 = conflict error code
+      } else {
+        bcrypt
+          .hash(password, 10)
+          .then(async (hash) => {
+            // enter user into the database
+            connection.query(
+              `INSERT INTO users (email, password) VALUES ('${email}', '${hash}');`,
+              (err, results) => {
+                if (err) return res.status(409).json({ error: err });
+                exists = results;
+              }
+            );
+            return res.status(201).json({ message: "User created." });
+          })
+          .catch((error) => {
+            return res.status(500).json({ error: error });
+          });
+      }
     }
-
-})
+  );
+});
 
 module.exports = router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import LoginPage from './routes/LoginPage/LoginPage';
+import RegisterPage from './routes/RegisterPage/RegisterPage';
+import Dashboard from './routes/Dashboard/Dashboard';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+
+        <Route exact path='/' element={<LoginPage/>}/>
+        <Route exact path='/register' element={<RegisterPage/>}/>
+        <Route exact path='/dashboard' element={<Dashboard/>}/>
+       
+      </Routes>
+    </Router>
   );
 }
 
