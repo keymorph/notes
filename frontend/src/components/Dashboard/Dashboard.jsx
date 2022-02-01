9602612dc231dc104886232a09ba5caf40f0f027
Raciel <raciel2901@gmail.com>
2022-02-01 01:29:22 -0500

    chore(navbar): initial cleanup, improve toggle and add fourscript logo

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import Navbar from "../Navbar";
 import NoteTimeline from "./Note/NoteTimeline";
-import Note from "./Note/Note";
 import axios from "axios";
 
 export default function Dashboard() {
