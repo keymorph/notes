08ca15088fa52d54ff628c6f6662fabd7fa28743
Evan C. Navarro <EvanCNavarro@gmail.com>
2022-02-01 22:57:21 -0500

    feat(modal): created semi-broken modal for notes
    - Note modal that works, but breaks the expansion feature.
    - CONSIDER: removing the expansion feature, and just having the modal.
    - Refer to Evan & Dante about this.
    
    Co-Authored-By: Dante Edmonson <74576480+danteedmonson@users.noreply.github.com>
    Co-Authored-By: Raciel <39206666+Rikimbili@users.noreply.github.com>

--- frontend/src/components/Dashboard/Note/Note.jsx
+++ frontend/src/components/Dashboard/Note/Note.jsx
@@ -14,9 +14,17 @@ import {
   Menu,
   MenuItem,
   Button,
+  CardActionArea,
+  CardActions,
+  TextField,
+  Chip
 } from "@mui/material";
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import { DeleteIcon, EditIcon, MoreVert} from "@mui/icons-material";
+import { DeleteIcon, EditIcon, MoreHoriz} from "@mui/icons-material";
+import Modal from '@mui/material/Modal';
+import  './style.css'
+
+
 
 function Note(props) {
   
@@ -28,6 +36,32 @@ function Note(props) {
   const [contentHeight, setContentHeight] = useState("")
   const [checked, setChecked] = useState(false);
 
+  // MODAL
+  const [modalOpen, setModalOpen] = useState(false);
+  const handleOpen = () => setModalOpen(true);
+  const handleClose = () => {
+      setModalOpen(false);
+      setCardHeight(contentHeight)
+    };
+
+  // MODAL: Category (Chip)
+  const [chipCategory, deleteChip] = useState(false);
+  // Delete the category for the note, and update the database.
+  const handleChipDelete = () => { deleteChip(true) }   
+
+  // Style of the Modal (SHOULD BE CHANGED LATER ON TO FIT MUI-THEME)
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
   const ref = useRef(null)
 
   useEffect(() => {
@@ -35,7 +69,6 @@ function Note(props) {
     console.log(contentHeight)
     setCardHeight("200px")
   },[])
-
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -61,61 +94,109 @@ function Note(props) {
   }
 
   return (
-    <Collapse in={checked} collapsedSize={350}>
-    <Card
-      sx={{ maxWidth: "200px", minHeight: "200px",overflowWrap:"break-word", transition: "height 1s linear", margin:"5px"}}
-      style={{transform: scale, transition: "height 0.1s linear, transform 0.1s linear" , zIndex: 10, height: cardHeight, maxHeight:"600px"}}
-      onMouseOver={() => setScale("scale(1.02,1.02")}
-      onMouseLeave={() => setScale("scale(1, 1)")}
-      ref={ref}
-    >
-      
-      <IconButton sx={{position:"absolute", right:0, bottom:0, transform: flip, transition:"transform 0.2s linear"}} onClick={expandButton}>
-        <KeyboardArrowDownIcon></KeyboardArrowDownIcon>
-      </IconButton>
-
-
-      <Box sx={{backgroundColor: '#388a82', height: '30px', position: 'relative'}}>
-        {/* { props.category } */}
-        {/* NONE */}
-        <IconButton aria-label="settings" sx={{position:'absolute', right: '0px', padding: '0px'}} 
-          onClick={handleClick}
-        >
-          <MoreVert sx={{color: 'white'}}/>
-        </IconButton>
-      </Box>
-
-      <Menu 
-      
-        anchorEl={anchorEl}
-        open={open}
-        onClose={() => setAnchorEl(null)}
-      
+   
+      <Card
+        className="NoteCard"
+        sx={{ maxWidth: "200px", minHeight: "200px",overflowWrap:"break-word", transition: "height 1s linear", margin:"5px",  }}
+        style={{transform: scale, transition: "height 0.1s linear, transform 0.1s linear" , zIndex: 10, height: cardHeight, maxHeight:"600px", paddingTop: "0px !important"}}
+        onMouseOver={() => setScale("scale(1.02,1.02")}
+        onMouseLeave={() => setScale("scale(1, 1)")}
+        ref={ref}
       >
 
-        <MenuItem onClick={handleEdit}>Edit</MenuItem>
-        <MenuItem onClick={handleDuplicate}>Duplicate</MenuItem>
-        <MenuItem onClick={handleDelete}>Delete</MenuItem>
-
-
-      </Menu>
-
-      <Grid>
-        <Typography variant="subtitle1" title="Title Name">
-          {/* { props.title } */}
-          Grocery List
-        </Typography>
-      </Grid>
-      <Divider variant="middle" />
-      <CardContent onClick={() => setChecked( (prevState) => !prevState )} sx={{userSelect:'text'}}>
-        <Typography variant="body2" sx={{fontSize: "12px",}}>
-          {/* { props.description } */}
-          There are words, words, words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,and more words
-        </Typography>
-      </CardContent>
       
-    </Card>
-    </Collapse>
+        <Box sx={{backgroundColor: '#388a82', height: '40px', position:"relative" }}>
+          {/* { props.category } */}
+          {/* NONE */}
+          <Chip label="Category" />
+
+          <IconButton aria-label="settings" sx={{position:'absolute', right: '0px', padding: '0px'}} 
+            onClick={handleClick}
+          >
+            <MoreHoriz sx={{color: 'white'}}/>
+          </IconButton>
+        </Box>
+
+        <Menu 
+          anchorEl={anchorEl}
+          open={open}
+          onClose={() => setAnchorEl(null)}
+        >
+          <MenuItem onClick={handleEdit}>Edit</MenuItem>
+          <MenuItem onClick={handleDuplicate}>Duplicate</MenuItem>
+          <MenuItem onClick={handleDelete}>Delete</MenuItem>
+        </Menu>
+
+        <CardActionArea onClick={handleOpen}>
+          <CardContent sx={{userSelect:'text'}}>
+            <Grid>
+              <Typography variant="subtitle1" title="Title Name">
+                {/* { props.title } */}
+                Grocery List
+              </Typography>
+            </Grid>
+
+            <Divider variant="middle" />
+
+            <Typography variant="body2" sx={{fontSize: "12px",}}>
+              {/* { props.description } */}
+              There are words, words, words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,words, words,and more words
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+
+        <IconButton sx={{position:"absolute", right:0, bottom:0, transform: flip, transition:"transform 0.2s linear"}} onClick={expandButton}>
+          <KeyboardArrowDownIcon></KeyboardArrowDownIcon>
+        </IconButton>
+
+        <CardActions>
+          <Modal
+            open={modalOpen}
+            onClose={handleClose}
+            onBackdropClick={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography variant="h5">
+                EDIT NOTE:
+              </Typography>
+              {/* Note Modal: TITLE Field */}
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                <TextField
+                  required
+                  id="outlined-required"
+                  label="Title"
+                  defaultValue="{props.title}"
+                />
+              </Typography>
+
+              {/* Note Modal: DESCRIPTION Field */}
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                <TextField
+                  id="outlined-multiline-static"
+                  label="Description"
+                  multiline
+                  rows={4}
+                  defaultValue="{props.description}"
+                />
+              </Typography>
+
+              {/* Note Modal: CATEGORY (Chips) Field */}
+              <Typography>
+                <Chip
+                  label="{props.category}"
+                  onDelete={handleChipDelete}
+                />
+              </Typography>
+
+            </Box>
+          </Modal>
+        </CardActions>
+
+    
+      </Card>
+
   );
 }
 
