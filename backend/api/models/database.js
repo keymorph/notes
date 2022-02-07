d70eab8e30226c0c02a4a88ca4a337883b5a92d5
Raciel <raciel2901@gmail.com>
2022-02-06 22:21:52 -0500

    refactor(database): minor cleanup

--- backend/api/models/database.js
+++ backend/api/models/database.js
@@ -1,27 +1,25 @@
 import { CosmosClient } from "@azure/cosmos";
 
-const [endpoint, key] = [process.env.COSMOSDB_ENDPOINT, process.env.COSMOSDB_KEY];
-const client = new CosmosClient({
-    endpoint: endpoint,
-    key: key
-});
+const config = {
+    endpoint: process.env.COSMOSDB_ENDPOINT,
+    key: process.env.COSMOSDB_KEY
+}
+const client = new CosmosClient(config);
 
 // Get database
-const databaseId = process.env.COSMOS_DB_NAME;
 const { database } = await client.databases.createIfNotExists({ id: process.env.COSMOSDB_DATABASE})
   .catch(err => {
       console.error('Error getting the database:\n', err);
   });
 
 // Get users container items
-const [usersContainerId, notesContainerId] = [process.env.COSMOSDB_USERS_CONTAINER, process.env.COSMOSDB_NOTES_CONTAINER];
-const { usersContainer } = await database.containers.createIfNotExists({ id: usersContainerId })
+const { usersContainer } = await database.containers.createIfNotExists({ id: process.env.COSMOSDB_USERS_CONTAINER })
     .catch(err => {
         console.error(err);
     });
 
 // Get notes container items for jotfox
-const { notesContainer } = await database.containers.createIfNotExists({ id: notesContainerId })
+const { notesContainer } = await database.containers.createIfNotExists({ id: process.env.COSMOSDB_NOTES_CONTAINER })
     .catch(err => {
         console.error(err);
     });
