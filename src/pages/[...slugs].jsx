dae9b4909b220a91a2681a2a832eaa8704a36ea6
Rikimbili <raciel2901@gmail.com>
2022-07-17 17:43:59 -0400

    fix(slugs): Only use router on the client-side

--- src/pages/[...slugs].jsx
+++ src/pages/[...slugs].jsx
@@ -1,10 +1,14 @@
 /*
-  Catch all possible routes and redirect the user to dashboard.
-  Predefined routes take precedence so this won't override them.
+  Catch all possible routes and redirect the user to the root slug.
+  Any predefined route takes precedence so this won't override them.
 */
 import {useRouter} from "next/router";
+import {useEffect} from "react";
 
 export default function AnySlug() {
   const router = useRouter();
-  router.replace("/");
+
+  useEffect(() => {
+    router.replace("/");
+  }, [router]);
 }
